d4bf76052cf67e1802d1825a140c018dac103d50
Mohamed Amine Bessrour <medaminbes03@outlook.com>
2024-07-07 21:36:51 -0400

    Hosting update router basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Services from './Services'; // Import the Services component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/trendtide">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/contact" element={<Contact />} />
